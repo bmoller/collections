c504f2d5f12c138d3b29d2c7d312ef21e8a82259
Brandon Moller <brandon@bkmoller.net>
2022-11-17 20:11:28 -0700

    godoc comments for collections.go

--- collections.go
+++ collections.go
@@ -7,19 +7,46 @@ import (
 
 // Collection
 
+/*
+A Collection represents an arbitrary group of values.
+*/
 type Collection[T comparable] interface {
-	Empty() bool
-	Size() int
+	Empty() bool // Indicates if the Collection is empty
+	Size() int   // Returns the number of values in the Collection
 }
 
-// Iterator
+// Iterable
 
+/*
+An Iterable returns an Iterator to navigate its elements.
+*/
+type Iterable[T comparable] interface {
+	Iterator() Iterator[T]
+}
+
+/*
+An Iterator can be used to loop through all of the elements of the returning Iterable.
+*/
 type Iterator[T comparable] func() (T, error)
 
+/*
+ErrNoMoreItems indicates that an iterator has returned all of its items.
+Callers should be prepared for this error and treat it as an expected state.
+*/
 var ErrNoMoreItems = errors.New("no more items to return")
 
 // List
 
+/*
+A List is an ordered Collection of items.
+Values added to the list retain the order in which they are added, and can be accessed by index.
+New elements can be inserted at an arbitrary index or added to the end of the list.
+Elements can be removed individually by index, or all at once with a call to Clear.
+
+A List can also return a subset of its values via a call to SubList.
+Similar to a slice, a SubList is created by referencing a range of indexes of the originating list.
+However, a SubList is not another view into the same values, but instead is a complete copy of the elements in the range specified.
+*/
 type List[T comparable] interface {
 	Collection[T]
 
@@ -31,20 +58,29 @@ type List[T comparable] interface {
 	SubList(int, int) (List[T], error)
 }
 
+/*
+ErrEmptyList indicates that the list is empty and the method called is not available.
+*/
 var ErrEmptyList = errors.New("list is empty")
 
+/*
+ErrIndexOutOfRange is returned when a method is called with an index beyond the list's size.
+*/
 type ErrIndexOutOfRange struct {
-	Index int
-	Size  int
+	Index int // Index requested in the method call
+	Size  int // Size of the list
 }
 
 func (e ErrIndexOutOfRange) Error() string {
 	return fmt.Sprintf("index %d is invalid for list of length %d", e.Index, e.Size)
 }
 
+/*
+ErrInvalidRange is returned if a range specifies an index less than 0, or if the End index is less than the Start index.
+*/
 type ErrInvalidRange struct {
-	End   int
-	Start int
+	End   int // End index of the requested range
+	Start int // Start index of the requested range
 }
 
 func (e ErrInvalidRange) Error() string {
@@ -53,12 +89,31 @@ func (e ErrInvalidRange) Error() string {
 
 // LinkedList
 
+/*
+A ListNode stores a single element of a LinkedList.
+In addition to the element's value the node also stores references to the next and previous nodes in the list, allowing for traversal in either direction.
+In general, a call to Next or Previous will return a pointer to a concrete implementation of the ListNode interface.
+A LinkedList also stores references to several of its key nodes, such as the head and tail.
+*/
 type ListNode[T comparable] interface {
 	Next() ListNode[T]
 	Previous() ListNode[T]
 	Value() T
 }
 
+/*
+A LinkedList implements the List interface with an extra set of methods to increase versatility for traversal and element management.
+The list is composed of a group of ListNodes that are members of the LinkedList.
+References are always maintained to the first and last nodes in the list for rapid addition of new items.
+
+Once a reference to any node is obtained, the list can be traversed to any other node by repeated calls to Next or Previous.
+However, nodes do not necessarily know their positions in the larger list, though the order in which they are added is retained.
+
+LinkedLists can add elements directly before or after an existing node.
+Existing nodes can also be directly removed, in which case the previous and next nodes (if any) are linked to each other.
+
+As a general rule, LinkedLists are slower than Lists for any index-based operations as the nodes must be traversed to reach the required element.
+*/
 type LinkedList[T comparable] interface {
 	List[T]
 
@@ -70,12 +125,24 @@ type LinkedList[T comparable] interface {
 	Tail() ListNode[T]
 }
 
+/*
+ErrNodeIsNotElement is returned if a method is called with a reference to a node that is not in the LinkedList.
+*/
 var ErrNodeIsNotElement = errors.New("node is not an element of this list")
 
+/*
+ErrWrongNodeType indicates that a referenced node is from an incompatible concrete implementation of LinkedList.
+Most methods of nodes and lists reference interface types in their signatures, but rely on specific plumbing in their implementation.
+*/
 var ErrWrongNodeType = errors.New("node is from an incompatible list implementation")
 
 // Queue
 
+/*
+A Queue represents a First In, First Out data structure.
+New elements are added to the end of the queue and will be returned after all preceding items.
+Values are returned and removed from the Queue via Pop, or a value can be retrieved without removal via Peek.
+*/
 type Queue[T comparable] interface {
 	Collection[T]
 
@@ -84,21 +151,34 @@ type Queue[T comparable] interface {
 	Push(T)
 }
 
+/*
+ErrEmptyQueue is returned when Peek or Pop is called on a Queue with no elements.
+This should be considered a part of normal operations and callers should expect to handle the error.
+*/
 var ErrEmptyQueue = errors.New("queue is empty")
 
 // Set
 
+/*
+A Set guarantees that exactly one instance of a value is present when added.
+If the value is already a member of the Set then Add is a no-op.
+The Set can be queried for a specific value and can remove values.
+The Remove method never returns an error; it only ensures that the element is not present in the Set.
+An indeterminate value can also be removed and returned with a call to Pop.
+*/
 type Set[T comparable] interface {
 	Collection[T]
+	Iterable[T]
 
 	Add(T)
 	Contains(T) bool
-	Iterator() Iterator[T]
 	Pop() (T, error)
 	Remove(T)
 }
 
-// ErrEmptySet is returned when Peek or Pop are called on an empty Set.
+/*
+ErrEmptySet is returned when Peek or Pop are called on an empty Set.
+*/
 var ErrEmptySet = errors.New("stack is empty")
 
 // Stack
